e193f38694dc0e85d936b5d87d5edc0d41bdf17c
luisdamces <luis.utrilla@cesjuanpablosegundo.es>
2025-02-10 12:32:23 +0100

    line de puntos

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 </head>
 <body>
     <h1>REPASO CRUD MYSQL PHP</h1>
+    <h2>---------------------</h2>
     <?php
         //CONEXIÓN
         $conexion = mysqli_connect("localhost", "root", "", "accesodatos") or
